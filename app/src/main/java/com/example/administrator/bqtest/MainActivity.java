150e385082168c61ac7818214d39fb9076964d57
vitozhang <developervito@163.com>
2017-02-15 11:42:21 +0800

    测试提交到booqin

--- app/src/main/java/com/example/administrator/bqtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/bqtest/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         findViewById(R.id.oim_bt).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
