3e4d93266a5e3b63a064b73e30302343ba0d5179
vitozhang <developervito@163.com>
2017-02-15 11:12:10 +0800

    测试提交到booqin

--- app/src/main/java/com/example/administrator/bqtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/bqtest/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.fresco_bt).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
